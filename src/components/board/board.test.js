465f4ac2292b538b8b89550c7c175db1d410b4c0
Ben Wiedermann <benw@cs.hmc.edu>
2020-03-05 00:13:22 -0800

    refactor: factor out board component

---
+++ src/components/board/board.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './board';
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Board squares={Array(9).fill(null)}></Board>, div);
+  }
+);
